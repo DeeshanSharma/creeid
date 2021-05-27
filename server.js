422b5fa6ca1c63018ab05fd8ed1db1af64317d37
Deeshan Sharma <deeshansharma04@gmail.com>
2021-05-27 13:52:08 +0530

    ✨ feat: database and api connected and working

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const IdeasAPI = require("./routes/api/ideas");
+const connectDB = require("./database/CreeidDB");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api", IdeasAPI);
+
+connectDB();
 
 app.get("/", (req, res) => res.send("Server up and running"));
 

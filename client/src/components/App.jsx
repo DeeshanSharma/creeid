ad1e96d324691e015f98e85933bafc94022d5ea8
Deeshan Sharma <deeshansharma04@gmail.com>
2021-05-27 21:15:55 +0530

    ✨ feat: add client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import ColorModeSwitcher from "./ColorModeSwitcher";
+
+function App() {
+	return (
+		<ChakraProvider theme={theme}>
+			<ColorModeSwitcher />
+		</ChakraProvider>
+	);
+}
+
+export default App;

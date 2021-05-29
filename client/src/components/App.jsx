1cdeb5ef4cade0ce10ca031a5dc65b21b34b628b
Deeshan Sharma <deeshansharma04@gmail.com>
2021-05-29 17:28:29 +0530

    ✨ feat: display all ideas fetched from database

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ChakraProvider, theme } from "@chakra-ui/react";
-import ColorModeSwitcher from "./ColorModeSwitcher";
+// import { ChakraProvider, theme } from "@chakra-ui/react";
+// import ColorModeSwitcher from "./ColorModeSwitcher";
 import Home from "./Home";
 
 function App() {
 	return (
-		<ChakraProvider theme={theme}>
-			<ColorModeSwitcher />
+		// <ChakraProvider theme={theme}>
+		// 	<ColorModeSwitcher />
 			<Router>
 				<Switch>
 					<Route exact path="/" component={Home} />
 				</Switch>
 			</Router>
-		</ChakraProvider>
+		// </ChakraProvider>
 	);
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Idea from "./Idea";
 
 function Home() {
+	const [ideas, setIdeas] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("/api/ideas")
+			.then((res) => setIdeas(res.data))
+			.catch((err) => console.log(err.response.data.error));
+	}, []);
+
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<>
+			<h1>All Ideas</h1>
+			{ideas.map((idea) => {
+				return <Idea key={idea._id} idea={idea} />;
+			})}
+		</>
 	);
 }
 

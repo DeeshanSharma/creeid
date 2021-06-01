c8045b65b7f271dd88dea54b5db2d65a715a3e05
Deeshan Sharma <deeshansharma04@gmail.com>
2021-06-01 09:48:57 +0530

    ✨ feat: add same page update functionality

--- client/src/components/Idea.jsx
+++ client/src/components/Idea.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Thread from "./Thread";
 
 function Idea(props) {
+	const [idea, setIdea] = useState(props.idea);
+	const [toggleEdit, setToggleEdit] = useState(false);
+
+	function onChange(event) {
+		const { name, value } = event.target;
+
+		setIdea((prevValues) => {
+			return {
+				...prevValues,
+				[name]: value,
+			};
+		});
+	}
+
+	function onSubmit(event) {
+		event.preventDefault();
+		setToggleEdit(false);
+
+		axios
+			.patch(`/api/update/${idea._id}`, idea)
+			.then((res) => {
+				console.log(res.data.updated);
+			})
+			.catch((err) => console.log(err));
+	}
+
 	function onDelete(id) {
 		axios
 			.delete(`/api/delete/${id}`)
@@ -13,18 +39,39 @@ function Idea(props) {
 			})
 			.catch((err) => console.log(err.response.data.notDeleted));
 	}
+
 	return (
 		<>
-			<h1>{props.idea.title}</h1>
-			<span>{new Date(props.idea.date).toLocaleString("en-US", { weekday: "short", year: "numeric", month: "short", day: "numeric" })}</span>
-			<p>{props.idea.description}</p>
-			{props.idea.thread.length !== 0 &&
-				props.idea.thread.map((thread) => {
-					return <Thread key={thread._id} thread={thread} ideaId={props.idea._id} />;
-				})}
-			<Link to={`/ideas/${props.idea._id}/thread/new`}>New Thread</Link>
-			<Link to={`/update/${props.idea._id}`}>Update</Link>
-			<button onClick={() => onDelete(props.idea._id)}>Delete</button>
+			{!toggleEdit ? (
+				<div onDoubleClick={() => setToggleEdit(true)}>
+					<h1>{idea.title}</h1>
+					<span>{new Date(idea.date).toLocaleString("en-US", { weekday: "short", year: "numeric", month: "short", day: "numeric" })}</span>
+					<p>{idea.description}</p>
+					{idea.thread.length !== 0 &&
+						idea.thread.map((thread) => {
+							return <Thread key={thread._id} thread={thread} ideaId={idea._id} />;
+						})}
+					<Link to={`/ideas/${idea._id}/thread/new`}>New Thread</Link>
+					<button onClick={() => onDelete(idea._id)}>Delete</button>
+				</div>
+			) : (
+				<form
+					autoComplete="off"
+					onSubmit={(event) => onSubmit(event)}
+					onKeyDown={(event) => {
+						if (event.key === "Escape") {
+							setToggleEdit(false);
+							event.preventDefault();
+							event.stopPropagation();
+						}
+					}}>
+					<label htmlFor="title">Title:</label>
+					<input type="text" autoFocus={true} name="title" value={idea.title} onChange={(event) => onChange(event)} />
+					<label htmlFor="description">Description:</label>
+					<textarea name="description" rows="5" cols="50" value={idea.description} onChange={(event) => onChange(event)} />
+					<button type="submit">Update</button>
+				</form>
+			)}
 		</>
 	);
 }

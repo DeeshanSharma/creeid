23efe06cee70063135950ee3f9a700d1239ced45
Deeshan Sharma <deeshansharma04@gmail.com>
2021-06-08 22:12:11 +0530

    ✨ feat: add route links to the app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import { ChakraProvider, theme } from "@chakra-ui/react";
-// import ColorModeSwitcher from "./ColorModeSwitcher";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import Home from "./Home";
+import AllIdeas from "./AllIdeas";
 
 function App() {
 	return (
-		// <ChakraProvider theme={theme}>
-		// 	<ColorModeSwitcher />
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Home} />
-			</Switch>
-		</Router>
-		// </ChakraProvider>
+		<ChakraProvider theme={theme}>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/ideas" component={AllIdeas} />
+				</Switch>
+			</Router>
+		</ChakraProvider>
 	);
 }
 
